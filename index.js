144e662a11f273a47aa1f477e6cea1a9240acbe9
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-08-15 23:58:36 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return "This one is on me!"
+  }
+  if (ride > 2000 && ride <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity) {
+  return destinationCity == "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(reply) {
+  switch (reply) {
+    case 'generous':
+      return `Thank you so much.`;
+    case 'not as generous':
+      return `Thank you.`;
+    default:
+      return `Bye.`;
+  }
 }
